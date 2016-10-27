8ea6b8c86bdda88c5795dc7c63e5da5bc7d9918c
Chethan <crajendra@qwinix.io>
2016-10-27 17:10:53 +0530

    last

--- app1/app/models/user.rb
+++ app1/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+   attr_accessor :password_salt
+   before_save :encrypt_password
   
- 
+ def encrypt_password
+    if @Password.present?
+      password_salt = BCrypt::Engine.generate_salt
+      self.password = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
